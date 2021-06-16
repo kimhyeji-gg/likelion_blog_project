30f7870ccf9bf4cb868138527ff3e6df0f9b5a62
kimhyeji-gg <wl159753@naver.com>
2021-06-16 17:51:15 +0900

    aa

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tc&+5e-z$z2p^n(xf!(jdj)i&r$5$tjw$(wi)id0yf^h3l5g3*'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-tc&+5e-z$z2p^n(xf!(jdj)i&r$5$tjw$(wi)id0yf^h3l5g3*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG', 'True') != 'False')
 
 ALLOWED_HOSTS = ['*']
 
@@ -137,3 +137,4 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
